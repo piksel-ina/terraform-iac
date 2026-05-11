01b0f4e5fa11b1f49391725adf8e00e74516d9fc
taufik-shf <taufik.shf@gmail.com>
2026-05-11 16:28:23 +0700

    increase CloudFront cache TTL for OWS (1h/24h/7d)

--- applications/odc.tf
+++ applications/odc.tf
@@ -364,9 +364,9 @@ resource "aws_cloudfront_distribution" "ows_cache" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    min_ttl                = 3600
+    default_ttl            = 86400
+    max_ttl                = 604800
   }
 
   restrictions {
